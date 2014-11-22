764170000027277285721db2304897f763f8569a
MYDS <myds2@hotmail.com>
2014-11-22 16:00:53 -0500

    my R file with documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,33 @@
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+##This file includes two functions: makeCacheMatrix, cacheSolve
+
+#declare and define the function makeCacheMatrix
+
+makeCacheMatrix <- function(x = matrix()) {  # create a new environment
+  m <- NULL             # create a local variable m
   set <- function(y) {
-    x <<- y
-    m <<- NULL
+    x <<- y            # use the "superAssignment" operator to assign y to x
+    m <<- NULL         # assign value to variable m declared outside this function
   }
   get <- function() x
-  setinverse <- function(solve) m <<- solve
-  getinverse <- function() m
-  list(set = set, get = get,
+  setinverse <- function(solve) m <<- solve   # compute the inverse matrix 
+  getinverse <- function() m                  # get the inverse matrix
+  list(set = set, get = get,                  # create a list of four functions
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
-cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
+#declare and define the function cacheSolve
+
+# create a function to compute inverse matrix or get from cache
+
+cacheSolve <- function(x, ...) { 
+  m <- x$getinverse()              # get value of m from getinverse function
+  if(!is.null(m)) {                # if m is not null, print message and
     message("getting cached data")
-    return(m)
+    return(m)                      # return the m value from cache
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+  data <- x$get()                  # if m is null, compute the inverse matrix
+  m <- solve(data, ...)            # compute inverse matrix
+  x$setinverse(m)                  
+  m                                # return value of m
 }
